9d7c2b05ca62ed27bc79a31dcbae241b1ed2a9d9
JackSmart1234 <dodoankhac@gmail.com>
2022-12-17 16:06:42 +0700

    add button effects

--- src/components/ButtonsBox.js
+++ src/components/ButtonsBox.js
@@ -3,8 +3,8 @@ import './ButtonsBox.css';
 function ButtonsBox() {
   return (
     <div className="ButtonsBox">
-        <button className="type-one-button">+</button>
-        <button className="type-one-button">-</button>
+        <button className="type-one-button bigger-font">+</button>
+        <button className="type-one-button bigger-font">-</button>
         <button className="type-one-button">history</button>
         <button className="type-one-button">delete</button>
     </div>
